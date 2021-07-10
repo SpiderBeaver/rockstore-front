03a274a07f4da4dfeb0c25d193a13a407a6a89b5
SpiderBeaver <spdrbeaver@gmail.com>
2021-07-10 19:21:02 +0300

    Make the whole product card clickable.

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -13,21 +13,23 @@ export default function ProductsList({ products }: ProductsListProps) {
     <ul className={styles.productsList}>
       {products.map((product) => (
         <li key={product.id} className={styles.productsListItem}>
-          <div className={styles.productImageContainer}>
-            <Image
-              src={`http://localhost:3001/uploads/${product.pictureFilename}`}
-              alt={`Picture of ${product.name}`}
-              width={100}
-              height={100}
-              layout="responsive"
-            ></Image>
-          </div>
-          <div className={styles.productDetails}>
-            <Link href={`/products/${product.id}`}>
-              <a className={styles.productName}>{product.name}</a>
-            </Link>
-            <span className={styles.price}>${product.price.toFixed(2)}</span>
-          </div>
+          <Link href={`/products/${product.id}`}>
+            <a className={styles.productCard}>
+              <div className={styles.productImageContainer}>
+                <Image
+                  src={`http://localhost:3001/uploads/${product.pictureFilename}`}
+                  alt={`Picture of ${product.name}`}
+                  width={100}
+                  height={100}
+                  layout="responsive"
+                ></Image>
+              </div>
+              <div className={styles.productDetails}>
+                <span className={styles.productName}>{product.name}</span>
+                <span className={styles.price}>${product.price.toFixed(2)}</span>
+              </div>
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
